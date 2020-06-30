e9ab7a42b9f83ac10604d96482a5d4ed19d3b830
Allie A <alliejarsenault@gmail.com>
2020-06-30 19:12:10 -0400

    basic setup

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <h3>navigation</h3>
+    </div>
+  )
+};
+
+export default Navigation;
---
+++ src/components/PageWrapper/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../../components/Navigation/index.js';
+
+const PageWrapper = () => {
+
+  return (
+    <div>
+      <Navigation/>
+      <h1>Some sort of welcome message / image</h1>
+    </div>
+  )
+};
+
+export default PageWrapper;
